dcad974674ac0250bd5e210654e5192c4525510f
aad3sh <mallya.aa@northeastern.edu>
2022-09-23 14:01:45 -0400

    Minor changes to App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import React, {useState, useEffect} from "react";
 function App() {
   return (
     <div className="App">
+      <html>
+      <body>
       <h1>Web Dev Assignments</h1>
       <ul>
-        <li><a href="labs/a2/index.html">Assignment 2 Labs</a></li>
-        <li><a href="tuiter/navigation.html">Tuiter</a></li>
+          <li><a href="labs/a2/index.html">Assignment 2 Labs</a></li>
+          <li><a href="tuiter/navigation.html">Tuiter</a></li>
       </ul>
+      </body>
+      </html>
     </div>
   );
 }
