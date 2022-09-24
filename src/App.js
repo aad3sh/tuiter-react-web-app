b4a5f57e37c04797dce09469b67b2ccf3f0cc6ff
aad3sh <mallya.aa@northeastern.edu>
2022-09-23 22:03:33 -0400

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import React, {useState, useEffect} from "react";
+import React, {} from "react";
 
 function App() {
   return (
       <div className="App">
-          <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                  Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-              >
-                  Learn React
-              </a>
-          </header>
+          {/*<header className="App-header">*/}
+          {/*    <img src={logo} className="App-logo" alt="logo" />*/}
+          {/*    <p>*/}
+          {/*        Edit <code>src/App.js</code> and save to reload.*/}
+          {/*    </p>*/}
+          {/*    <a*/}
+          {/*        className="App-link"*/}
+          {/*        href="https://reactjs.org"*/}
+          {/*        target="_blank"*/}
+          {/*        rel="noopener noreferrer"*/}
+          {/*    >*/}
+          {/*        Learn React*/}
+          {/*    </a>*/}
+          {/*</header>*/}
+          {/*<h1>Web Dev Assignments</h1>*/}
+          {/*<ul>*/}
+          {/*    <li><a href="labs/a2/index.html">Assignment 2 Labs</a></li>*/}
+          {/*    <li><a href="tuiter/navigation.html">Tuiter</a></li>*/}
+          {/*</ul>*/}
       </div>
   );
 }
